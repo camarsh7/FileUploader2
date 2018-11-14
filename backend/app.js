738c2b1240e49a1c54836b002e83789ee8072fa9
camarsh7 <ultralays@hotmail.com>
2018-11-13 19:44:39 -0700

    US#22 Task#30, Task#31, Task#33

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,88 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const multer = require('multer');
 const app = express();
 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/test', (req, res) => {
-    return res.send({ test: 'testing' });
+// configure multer dest and file names
+var storage = multer.diskStorage({
+  // destination
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  }
 });
 
-app.post('/upload', (req, res) => {
-    res.send('I received your POST. This is what you sent me: ${req.body.post}'
-    );
+// initiatie multer
+const upload = multer({ storage: storage });
+
+app.post('/upload', (req, res, next) => {
+  //console.log(req);
+	console.log(req.files.file);
+	var imageFile;
+ 
+	  imageFile = req.files.file;
+
+	  imageFile.mv(`${__dirname}/public/uploads/${req.files.file.name}`, function(err) {
+		if (err) {
+		  return res.status(500).send(err);
+		}
+
+		res.json({file: `public/uploads/${req.files.file.name}`});
+	  });
+  
+
+})
+
+/* app.post('/upload', upload.single('files'), (req, res) => {
+	//console.log(req);
+	console.log(req.body.file);
+
+  
+}); */ 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(process.env.PORT || 8080);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8000, () => {
+  console.log('8000');
+});
+
+module.exports = app;
